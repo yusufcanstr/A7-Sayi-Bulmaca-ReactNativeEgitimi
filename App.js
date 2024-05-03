f6df604cec324122c88816f720a57342deff6d6b
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-03 12:15:16 +0300

    LineerGradient Ekleme

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import GameStartScreen from "./screens/GameStartScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <GameStartScreen/>
+    <LinearGradient
+      style={styles.container}
+      colors={["rgba(0,0,0,0.8)", "transparent"]}
+    >
+      <GameStartScreen />
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
