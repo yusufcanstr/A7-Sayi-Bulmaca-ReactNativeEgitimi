95d0d76b0ec7958d1d728f7197e2ee4fc5175bb7
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-03 10:48:15 +0300

    Proje Açılışı ve Ekranların Açılması

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CustomButton from '../components/CustomButton'
+
+export default function GameOverScreen() {
+  return (
+    <View>
+      <Text>GameOverScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
