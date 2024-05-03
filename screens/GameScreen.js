592ff1184cf876a7c5c5fc434b4d72d2db8dc1cf
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-03 22:23:08 +0300

    Bilgisayarın Başlangıç Sayı Tahmini

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,22 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Title from '../components/Title'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import Title from "../components/Title";
+import ComputerNumber from "../components/ComputerNumber";
 
-export default function GameScreen() {
+export default function GameScreen({ userNumber }) {
+  const initialGuess = generateNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  function generateNumber(min, max, exclude) {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+    if (randomNumber === exclude) {
+      return generateNumber(min, max, exclude);
+    } else {
+      return randomNumber;
+    }
+  }
   return (
     <View style={styles.container}>
       <Title>Bilgisayar Tahmini</Title>
+      <ComputerNumber>{currentGuess}</ComputerNumber>
       <View>
         <Text>Altında mı üstünde mi ?</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 30,
-    
-  }
-})
+  },
+});
